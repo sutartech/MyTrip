220564a4885f748b34d7979fe697b2213b801d8b
kkjohn <63163260+sutartech@users.noreply.github.com>
2026-09-17 23:24:09 +0530

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-/* Optional: paste your deployed Google Apps Script /exec URL below.
-   If left blank, the dashboard will ask the administrator to connect it. */
-window.MYTRIP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxV0aeergqd7Ek9MhdI-ZPW6D4eXH6TY-hmtZi1gaR-EXoSNzYTpmOkrGeYOr8AueVcKQ/exec",
-  APP_NAME: "MyTrip",
-  DEFAULT_CURRENCY: "INR"
-};
