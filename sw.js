18ad2e4295a617bc4bae8a30b27700be8495ef12
kkjohn <63163260+sutartech@users.noreply.github.com>
2026-09-20 20:55:14 +0530

    4.15.2

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const STATIC_CACHE = "mytrip-static-v4.15.1-speed2";
+const STATIC_CACHE = "mytrip-static-v4.15.2-speed2";
 const VERSIONED_ASSETS = [
   "./index.html",
-  "./styles.css?v=4.15.1-speed2",
-  "./app.js?v=4.15.1-speed2",
+  "./styles.css?v=4.15.2-speed2",
+  "./app.js?v=4.15.2-speed2",
   "./favicon.svg",
   "./repair.html"
 ];
