d481e266e592b6aa74c0557de28a795b25eacf2b
kkjohn <63163260+sutartech@users.noreply.github.com>
2026-09-21 15:41:03 +0530

    4.18.7
    sticky note save option always

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const STATIC_CACHE = "mytrip-static-v4.18.6-speed2";
+const STATIC_CACHE = "mytrip-static-v4.18.7-speed2";
 const VERSIONED_ASSETS = [
   "./index.html",
-  "./styles.css?v=4.18.6-speed2",
-  "./app.js?v=4.18.6-speed2",
+  "./styles.css?v=4.18.7-speed2",
+  "./app.js?v=4.18.7-speed2",
   "./favicon.svg",
   "./repair.html"
 ];
